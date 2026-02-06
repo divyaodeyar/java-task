72eb055e365d2b6f084f43867e2ee2e5905770f5
divyaodeyar <divyaodiyar115@gmail.com>
2026-02-06 22:32:08 +0530

    commit

---
+++ operators/OddEvenCheck.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class OddEvenCheck {
+    public static void main(String[] args) {
+        
+        int num = 7;
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
